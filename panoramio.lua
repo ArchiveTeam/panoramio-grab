b378dbb0f63c2b9d56539b92a90f076a06ba0d33
Arkiver2 <Arkiver@hotmail.com>
2014-10-05 15:18:54 +0200

    panoramio.lua: fix

--- panoramio.lua
+++ panoramio.lua
@@ -353,7 +353,7 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
               end
             end
           else
-            local base = string.match(url, "(http[s]?://[^/]+)")
+            local base = string.match(url, "(http[s]?://[^/]+)/")
             local customurl = base..customurlnf
             if downloaded[customurl] ~= true then
               table.insert(urls, { url=customurl })
@@ -403,7 +403,7 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
               end
             end
           else
-            local base = string.match(url, "(http[s]?://[^/]+)")
+            local base = string.match(url, "(http[s]?://[^/]+)/")
             local customurl = base..customurlnf
             if downloaded[customurl] ~= true then
               table.insert(urls, { url=customurl })
